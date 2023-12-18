b05e7eb5ddf1cca8edd2c5818ea2fa7267b1319c
dylanhamada <dylan.hamada@gmail.com>
2023-12-18 14:35:43 -0500

    Return First Two Stores and Modify One
    - Return first two stores in the stores table, and modify the name of the row with id 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,11 +5,13 @@ puts "----------"
 
 # Your code goes below here ...
 
+# create three new Active Record objects
 burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 
 richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 
 gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
+# count the number of rows in the stores table and print it to console
 store_count = Store.count
 puts "The total number of stores is: #{store_count}"
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# find the first two stores in the stores table
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+# modify the store name of the row with id 1
+@store1.update(name: "Vancouver")
