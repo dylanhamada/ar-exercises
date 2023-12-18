7e2e3e02c93d9f3f2964371eb051777750124b68
dylanhamada <dylan.hamada@gmail.com>
2023-12-18 17:15:18 -0500

    Return and Print Operations Involving Annual Revenue
    - Return the sum of the annual_revenue column and print it
    - Return the average of the annual_revenue column and print it
    - Return the number of rows that have annual_revenue of over 1000000 and print it

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -25,17 +25,8 @@ end
 
 # return all stores where womens_apparel is true and annual_revenue is less than 1000000
 @womens_stores = Store.where(womens_apparel: true)
+
+# iterate through @womens_stores and output their name and annual revenue
 @womens_stores.each do |store|
   puts "Store name: #{store.name} -- Annual revenue: #{store.annual_revenue}"
 end
-
-# Borrowing and modifying the code from Exercise one, create 3 more stores:
-# Surrey (annual_revenue of 224000, carries women's apparel only)
-# Whistler (annual_revenue of 1900000 carries men's apparel only)
-# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
-
-# Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable @mens_stores.
-
-# Loop through each of these stores and output their name and annual revenue on each line.
-
-# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# return the sum of the annual_revenue column and print it
+total_revenue = Store.sum(:annual_revenue)
+puts "The total revenue for the entire company is $ #{sprintf("%.2f", total_revenue)}."
+
+# return the average of the annual_revenue column and print it
+average_revenue = Store.average(:annual_revenue)
+puts "The average annual revenue for all stores is $ #{sprintf("%.2f", average_revenue)}."
+
+# return the number of rows that have annual_revenue of over 1000000 and print it
+million_plus_revenue = Store.where("annual_revenue >= ?", 1000000).count
+puts "There are #{million_plus_revenue} stores that have an annual revenue of over $ 1,000,000."
